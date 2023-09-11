817e258fa2e7bbbd497a702381ca3ea2609770bf
prajjwal <prajjwalpachauri725@gmail.com>
2023-09-11 16:36:35 +0530

    Initial commit

---
+++ src/main/java/com/example/login_spring_boot/LoginSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.login_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/login_spring_boot/LoginSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.login_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

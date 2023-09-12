4a7f368e6b0573d8debf0aed2b279458ff170794
prajjwal <prajjwalpachauri725@gmail.com>
2023-09-12 15:00:25 +0530

    Implemented UserService.java interface

--- src/main/java/com/example/login_spring_boot/service/UserService.java
+++ src/main/java/com/example/login_spring_boot/service/UserService.java
@@ -1,4 +1,10 @@
 package com.example.login_spring_boot.service;
 
+import com.example.login_spring_boot.model.User;
+
+import java.util.List;
+
 public interface UserService {
+    public void saveUser(User user);
+    public List<Object> isUserPresent(User user);
 }

91dea1bba0e5af7a6abb81f6ca12786ccf8af9b3
prajjwal <prajjwalpachauri725@gmail.com>
2023-09-12 20:30:34 +0530

    Change autowire annotation to constructor WebSecurityConfig.java class

--- src/main/java/com/example/login_spring_boot/config/WebSecurityConfig.java
+++ src/main/java/com/example/login_spring_boot/config/WebSecurityConfig.java
@@ -20,8 +20,11 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
-    @Autowired
-    private CustomLoginSuccessHandler successHandler;
+    private final CustomLoginSuccessHandler successHandler;
+
+    public WebSecurityConfig(CustomLoginSuccessHandler successHandler) {
+        this.successHandler = successHandler;
+    }
 
     @Bean
     public UserDetailsService userDetailsService() {

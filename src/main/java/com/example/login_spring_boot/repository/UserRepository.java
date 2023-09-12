b62ed36a7411450010014ff6cba4328723851e16
prajjwal <prajjwalpachauri725@gmail.com>
2023-09-12 14:57:21 +0530

    Implemented UserRepository.java interface

---
+++ src/main/java/com/example/login_spring_boot/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.login_spring_boot.repository;
+
+import com.example.login_spring_boot.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByMobile(String mobile);
+}

28e7e6867d2c77fa0e7f8f20bc4fb9232323bde1
prajjwal <prajjwalpachauri725@gmail.com>
2023-09-12 17:50:17 +0530

    Implemented a CustomLoginSuccessHandler.java class

--- src/main/java/com/example/login_spring_boot/config/CustomLoginSuccessHandler.java
+++ src/main/java/com/example/login_spring_boot/config/CustomLoginSuccessHandler.java
@@ -1,4 +1,42 @@
 package com.example.login_spring_boot.config;
 
-public class CustomLoginSuccessHandler {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Configuration
+public class CustomLoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+    @Override
+    protected void handle(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        String targetUrl = determineTargetUrl(authentication);
+        if (response.isCommitted()) return;
+        RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+        redirectStrategy.sendRedirect(request, response, targetUrl);
+    }
+
+    private String determineTargetUrl(Authentication authentication) {
+        String url = "/login?error=true";
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        List<String> roles = new ArrayList<>();
+        for (GrantedAuthority a : authorities) {
+            roles.add(a.getAuthority());
+        }
+        if (roles.contains("ADMIN")) {
+            url = "/admin/dashboard";
+        } else if (roles.contains("USER")) {
+            url = "/dashboard";
+        }
+        return url;
+    }
 }

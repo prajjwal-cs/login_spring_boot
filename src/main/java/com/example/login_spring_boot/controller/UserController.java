7feccd75e9b85ac42f802639c5d2d331d31a9082
prajjwal <prajjwalpachauri725@gmail.com>
2023-09-13 15:13:08 +0530

    Implemented UserController.java class

--- src/main/java/com/example/login_spring_boot/controller/UserController.java
+++ src/main/java/com/example/login_spring_boot/controller/UserController.java
@@ -1,4 +1,13 @@
 package com.example.login_spring_boot.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class UserController {
+    @RequestMapping(value = {"/dashboard"}, method = RequestMethod.GET)
+    public String homePage() {
+        return "user/dashboard";
+    }
 }

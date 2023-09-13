d340880e71f42e05b2fd45d49631399ca9f0e5f4
prajjwal <prajjwalpachauri725@gmail.com>
2023-09-13 15:08:04 +0530

    Implemented AdminController.java class

--- src/main/java/com/example/login_spring_boot/controller/AdminController.java
+++ src/main/java/com/example/login_spring_boot/controller/AdminController.java
@@ -1,4 +1,13 @@
 package com.example.login_spring_boot.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class AdminController {
+    @RequestMapping(value = {"/admin/dashboard"}, method = RequestMethod.GET)
+    public String adminHome() {
+        return "admin/dashboard";
+    }
 }
